f5bcda7e980490b282c6b36c6611d32308532593
DanteAguilarB <daguilarb6@gmail.com>
2025-09-05 10:59:45 -0700

    parser method leadFinder

--- index.js
+++ index.js
@@ -2,25 +2,55 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
 
-//Gathering starting five
-let MiddRoster = ["Witherington Edward", "Stevens Sam", "Brennan David", "Ramey Sawyer", "Urena Oliver"];
-let HamltnRoster = ["Morgan Hank", "Singh Teja", "Kane Owen", "	Robinson Graham", "Xu Cooper"];
+String.prototype.leadFinder = function() {
+  let string = this;
+  let lead = "";
+  let j = 1;
 
-fs.createReadStream('data.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    let subs = 0;
-    //Iterating over results
-    for(let i = 0; i < results.length; i++){
-        if(results[i]["Mid"].includes("SUB OUT") && results[i+1]["Mid"].includes("SUB IN")){
-            let MiddOut = results[i]["Mid"].replace("SUB OUT by ", "");
-            let MiddIn = results[i+1]["Mid"].replace("SUB IN by ", "");
-
-            //Updating the rosters
-            MiddRoster.pop(MiddOut);
-            MiddRoster.push(MiddIn);
-
+  for(i = 0; i < string.length; i++){
+    if(string[i] == "+"){
+      for(j = i+1; j < string.length; j++){
+        if(string[j] != ")") {
+          lead += string[j];
         }
+      }
+      break;
     }
-  });
+  }
+  return lead;
+}
+
+let test1 = "12(+23)";
+console.log(test1.leadFinder());
+
+//11(+23)
+//   i
+//    j
+//11
+
+//Gathering starting five
+// let MiddRoster = ["Witherington Edward", "Stevens Sam", "Brennan David", "Ramey Sawyer", "Urena Oliver"];
+// let HamltnRoster = ["Morgan Hank", "Singh Teja", "Kane Owen", "	Robinson Graham", "Xu Cooper"];
+
+// fs.createReadStream('data.csv')
+//   .pipe(csv())
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
+//     let subs = 0;
+//     //Iterating over results
+//     for(let i = 0; i < results.length; i++){
+        
+//         //Points
+//         if(results[i]["Mid"])
+
+//         //Substitutions
+//         if(results[i]["Mid"].includes("SUB OUT") && results[i+1]["Mid"].includes("SUB IN")){
+//             let MiddOut = results[i]["Mid"].replace("SUB OUT by ", "");
+//             let MiddIn = results[i+1]["Mid"].replace("SUB IN by ", "");
+
+//             //Updating the rosters
+//             MiddRoster.pop(MiddOut);
+//             MiddRoster.push(MiddIn);
+//         }
+//     }
+//   });
